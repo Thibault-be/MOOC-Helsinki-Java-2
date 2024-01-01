c46f895b27ac3b70248b2d6ace7327a45c0d0280
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-01 18:07:37 +0100

    Ex 8.2 AverageOfPositiveNumbers

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,31 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int count = 0;
+        while (true){
+            String inputString = scanner.nextLine();
+
+            try {
+                int input = Integer.valueOf(inputString);
+
+                if (input == 0) break;
+
+                if (input > 0){
+                    sum += input;
+                    count++;
+                }
+            } catch (Exception e) {
+                continue;
+            }
+
+
+        }
+        if (count == 0){
+            System.out.println("Could not calculate the average");
+        }else {
+            System.out.println(1.0 * sum / count);
+        }
+
     }
 }
