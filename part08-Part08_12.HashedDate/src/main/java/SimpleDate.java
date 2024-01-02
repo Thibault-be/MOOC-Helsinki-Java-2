eb997d8524dc5fe0b68661c2583087bab8cee4f8
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-02 23:40:36 +0100

    Ex 8.12  HashedDate

--- part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
+++ part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
@@ -51,4 +51,23 @@ public class SimpleDate {
         return this.year - other.year - yearRemoved;
     }
 
+    public int hashCode(){
+        String y = String.valueOf(this.year);
+        String m = String.valueOf(this.month);
+        String d = String.valueOf(this.day);
+        String concat = y+m+d;
+        return concat.hashCode();
+        
+    }
+
 }
+
+/*
+Let's expand the SimpleDate class from the previous exercise 
+to also have its own hashCode method.
+
+Create a method public int hashCode() for the SimpleDate class,
+ which calculates a hash for the the SimpleDate object. 
+ Implement the calculation of the hash in way that there are as few similar hashes 
+ as possible between the years 1900 and 2100.
+ */
