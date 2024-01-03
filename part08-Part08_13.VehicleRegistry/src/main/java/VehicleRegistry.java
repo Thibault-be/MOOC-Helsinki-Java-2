01edbba3751a243a3e19937feae98a2c51d3d042
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-03 16:47:37 +0100

    Remove comments in Ex 8.13

--- part08-Part08_13.VehicleRegistry/src/main/java/VehicleRegistry.java
+++ part08-Part08_13.VehicleRegistry/src/main/java/VehicleRegistry.java
@@ -12,10 +12,6 @@ public class VehicleRegistry {
   }
 
   public boolean add(LicensePlate licensePlate, String owner){
-    //assigns the owner it received as a parameter to a car that corresponds to the license plate received as a parameter. 
-    //If the license plate doesn't have an owner, the method returns true.
-    //If the license already has an owner attached, the method returns false and does nothing.
-    
     if (this.registry.get(licensePlate) == null){
       this.registry.put(licensePlate, owner);
       return true;
@@ -42,16 +38,12 @@ public class VehicleRegistry {
   }
 
   public void printOwners(){ 
-    //prints the owners of the cars in the registry.
-    //Each name should only be printed once, even if a particular person owns more than one car.
     ArrayList<String> owners = new ArrayList<>();
-    int count = 0;
     for (String key : this.registry.values()){
       if (!owners.contains(key)){
         owners.add(key);
       }
     }
-    System.out.println(count);
     for (String owner : owners){
       System.out.println(owner);
     }
