09db626bcd929f74d22a9878df8e842c00f542c7
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-09 11:40:35 +0100

    Part 10.2 AverageOfSelectedNumbers

--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -8,5 +8,53 @@ public class AverageOfSelectedNumbers {
         Scanner scanner = new Scanner(System.in);
         // toteuta ohjelmasi tänne
 
+
+/*
+Implement a program, which reads user input. If the user input is "end", program stops reading input. 
+The rest of the input is numbers.
+
+Then user is asked if the program should print the average of all the positive numbers, 
+or the average of all the negative numbers (n or p). If the user selects "n", 
+the average of all the negative numbers is printed. Otherwise the average of all the positive numbers is printed.
+
+*/
+        ArrayList<String> numbers = new ArrayList<>();
+
+        System.out.println("Put in some numbers. Type 'end' to quit.");
+
+        
+        while (true){
+
+            String input = scanner.nextLine();
+            if (input.equals("end")) break;
+            numbers.add(input);
+
+        }
+
+        System.out.println("Print out average of positive or negative numbers. Choose n or p.");
+        String  positiveOrNegative = scanner.nextLine();
+
+        double average = 0.0;
+        if (positiveOrNegative.equals("n")){
+            average = numbers.stream()
+                        .mapToInt(s -> Integer.valueOf(s))
+                        .filter(number -> number < 0)
+                        .average()
+                        .getAsDouble();
+        } else {
+            average = numbers.stream()
+                        .mapToInt(s -> Integer.valueOf(s))
+                        .filter(number -> number > 0)
+                        .average()
+                        .getAsDouble();
+        }
+        
+
+
+        System.out.println(average);
+
+
     }
+
+
 }
