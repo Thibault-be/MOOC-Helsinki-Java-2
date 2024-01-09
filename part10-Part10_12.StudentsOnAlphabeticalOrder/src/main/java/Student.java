e46efbbd78cb9b2a3edd457e10ba7c918e1cdd0c
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-09 16:21:19 +0100

    Part 10.12 StudentsOnAlphabeticalOrder

--- part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/Student.java
+++ part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/Student.java
@@ -1,6 +1,6 @@
 
 
-public class Student {
+public class Student implements Comparable<Student> {
 
     private String name;
 
@@ -17,4 +17,20 @@ public class Student {
         return name;
     }
 
+    @Override 
+    public int compareTo(Student student){
+        return this.name.compareToIgnoreCase(student.getName());
+
+    }
 }
+
+/*
+ * The exercise template includes the class Student, which has a name. 
+ Implement the Comparable interface in the Student class in a way, such that the compareTo method sorts the students in alphabetical order 
+ based on their names.
+
+The name of the Student is a String, which implements Comparable itself.
+You may use its compareTo method when implementing the method for the Student class. 
+Note that String.compareTo() also treats letters according to their size, while the compareToIgnoreCase method of the
+same class ignores the capitalization completely. You may use either of these methods in the exercise.
+ */
