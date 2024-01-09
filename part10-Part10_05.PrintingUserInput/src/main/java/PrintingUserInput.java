61b2cb5e960f29d09d58b6675e2aa4eb1ea18aea
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-09 13:54:44 +0100

    Part 10.5 PrintingUserInput

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -7,5 +7,18 @@ public class PrintingUserInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<String> userInput = new ArrayList<>();
+
+        while (true){
+
+            String input = scanner.nextLine();
+            if (input.equals("")) break;
+
+            userInput.add(input);
+        }
+        System.out.println("\n\n\n\n**************************");
+        userInput.stream()
+            .forEach(line -> System.out.println(line));
+
     }
 }
