e139098b61e0f4c0ae107098c908c1964cdbb735
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-09 17:11:55 +0100

    Part 10.13 LiteracyComparison - Started, not finished

--- part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -3,10 +3,25 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LiteracyComparison {
     
     public static void main(String[] args) {
 
+        try{
+            Files.lines(Paths.get("literacy.csv"))
+            .map(line -> line.split(","))
+            .sorted((line1, line2) -> Double.compare(Double.parseDouble(line1[5]), Double.parseDouble(line2[5])))
+            .forEach(line -> System.out.println(Arrays.toString(line)));
+        } catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
+
+
+//Create a program that first reads the file literacy.csv and 
+//then prints the contents from the lowest to the highest ranked on the literacy rate. 
+//The output must be exactly as in the following example. The example shows the first five of the expected rows.
