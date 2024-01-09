ac30cf42519dd1dbdf2cac4a8b1d27137c8b5576
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-09 23:01:07 +0100

    Part 10.15 Regex

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -2,4 +2,16 @@
 
 public class Checker {
 
+
+  public boolean isDayOfWeek(String string){
+    return string.matches("mon|tue|wed|thu|fri|sat|sun");
+  }
+  public boolean allVowels(String string){
+    return string.matches("^[aeiouAEIOU]+$");
+  }
+
+  public boolean timeOfDay(String string){
+    return string.matches("^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$");
+  }
+
 }
--- part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -1,9 +1,23 @@
-
+import java.util.Scanner;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Enter a string");
+            String check = scanner.nextLine();
+
+            Checker tue = new Checker();
+            System.out.println(tue.isDayOfWeek(check));
+        }
+
+        
+
+        
     }
 }
