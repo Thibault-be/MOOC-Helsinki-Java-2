104b7fc93412f3e30b55ef9a9c9e005564c80eaf
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-09 15:05:41 +0100

    Part 10.8 - weighting

--- part10-Part10_08.Weighting/src/main/java/Main.java
+++ part10-Part10_08.Weighting/src/main/java/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         // Use this main program for testing your classes!
+
+        System.out.println("The suitcases in the hold contain the following items");
+
+        
     }
 
 }
