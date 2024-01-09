5ddd991866df9e180725b6db9ce87ad87eb37521
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-09 11:22:56 +0100

    Part 10.1 - Streams AverageOfNumbers

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -8,5 +8,25 @@ public class AverageOfNumbers {
         Scanner scanner = new Scanner(System.in);
         // Write your program here
 
+        //Implement a program, which reads user input.
+        // If the user input is "end", the program stops reading input. 
+        //The rest of the input is numbers. When the user input is "end", the program prints the average of all of the numbers.
+
+        ArrayList<String> line  = new ArrayList<>();
+
+        while (true){
+            String input = scanner.nextLine();
+            if (input.equals("end")) break;
+            line.add(input);
+        }
+
+        double average = line.stream()
+                            .mapToInt(s -> Integer.valueOf(s))
+                            .average()
+                            .getAsDouble();
+
+        System.out.println(average);       
     }
+    
+
 }
