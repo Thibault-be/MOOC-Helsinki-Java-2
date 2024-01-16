f06c017ae863b52dbafa21f4563b203d6fda5b76
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-16 11:52:50 +0100

    Part 12.05 HashMap + 12.06 Numbers

--- part12-Part12_05.HashMap/src/main/java/Program.java
+++ part12-Part12_05.HashMap/src/main/java/Program.java
@@ -6,6 +6,8 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // You can test the class here
+        Pair<String, Integer> pair = new Pair<>("one", 1);
+System.out.println(pair.getKey() + " -> " + pair.getValue());
 
     }
 
--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("How many random numbers should be printed?");
+        int amount = Integer.valueOf(scanner.nextLine());
+
+        Random rand = new Random();
+        for (int i = 0; i < amount; i++){
+            System.out.println(rand.nextInt(11));
+        }
     }
 
 }
