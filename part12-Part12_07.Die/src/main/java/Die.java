5679bb309e728842a868d2e4d0b116e8c1ecb881
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-16 11:59:36 +0100

    Part 12.07 Die

--- part12-Part12_07.Die/src/main/java/Die.java
+++ part12-Part12_07.Die/src/main/java/Die.java
@@ -8,12 +8,15 @@ public class Die {
 
     public Die(int numberOfFaces) {
         this.random = new Random();
-        // Initialize the value of numberOfFaces here
+        this.numberOfFaces = numberOfFaces;
     }
 
     public int throwDie() {
+
+        return this.random.nextInt(numberOfFaces)
+        +1;
         // generate a random number which may be any number
         // between one and the number of faces, and then return it
-        return 0;
+        
     }
 }
